193bb2af08912e18bb2639be66ea81528fc8eb87
Pascal Encinas <krionari@gmail.com>
2019-05-12 14:43:38 +0200

    finalisation de la finalisation

--- index.php
+++ index.php
@@ -12,10 +12,11 @@ $timeTraveller = new TimeTravel(new DateTime('31-12-1985'), new DateTime());
 echo $timeTraveller->getTravelInfo();
 echo '</br>';
 
-echo $timeTraveller->findDate('retirer',new DateInterval('PT1000000000S'));
+$startDoc = $timeTraveller->findDate('retirer',new DateInterval('PT1000000000S'));
+echo $startDoc;
 echo '</br>';
 
-$start = new DateTime('31-12-1985');
+$start = new DateTime($startDoc);
 $end = new DateTime();
 $interval = new DateInterval('P1M8D');
 $days = $timeTraveller->backToFutureStepByStep(new DatePeriod($start, $interval, $end));
